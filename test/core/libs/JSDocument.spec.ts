12f8c7178eaeb0df30fcac4b41d065ff088824c9
differui <differui@gmail.com>
2018-12-06 16:20:06 +0800

    :fire: remove useless test cases

--- test/core/libs/JSDocument.spec.ts
+++
@@ -1,9 +0,0 @@
-import JSDocument from '@/core/libs/JsDocument';
-import * as identifier from '@/identifier';
-import container from '@/inversify.config';
-
-const jsDocument = container.get<JSDocument>(identifier.JSDocument);
-
-test('basic', () => {
-  // pass
-});
